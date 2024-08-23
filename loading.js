214960750086d1e25143ec83f5c490de44569c5b
Deviantejsh <158575481+Deviantejsh@users.noreply.github.com>
2024-08-23 21:44:50 +0200

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(function () {
+    document.getElementById('loading-screen').style.display = 'none';
+  }, 3000); // 5 секунд
+});
