83ffeb0afffd5f07523914f16f71523798ebb54d
Deviantejsh <158575481+Deviantejsh@users.noreply.github.com>
2024-08-23 21:41:18 +0200

    Delete loading.js

--- loading.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  setTimeout(function () {
-    document.getElementById('loading-screen').style.display = 'none';
-  }, 7000); // 7 секунд
-});
